1077ccbb8ad45b517d955318ce4570e578e1f81c
Your New Name <grierniu1511@gmail.com>
2024-10-09 09:39:01 +0530

    input page progressed

--- client/app/Pg2/page.js
+++ client/app/Pg2/page.js
@@ -2,62 +2,83 @@ import React from 'react';
 
 export default function Pg2() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen pt-10 bg-black text-white">
+        <div style={{ paddingTop: '400px' }} className="flex flex-col items-center justify-center h-screen bg-black text-white">
             <h1 className="text-white text-4xl mb-4">Build Your Own Website</h1>
-            <div className="flex flex-col space-y-4 w-full max-w-md">
-                {/* Initial five inputs */}
-                <input
-                    type="text"
-                    placeholder="Input 1"
-                    className="w-full p-4 text-lg rounded bg-gray-700 text-white min-h-[56px]"
-                />
-                <input
-                    type="text"
-                    placeholder="Input 2"
-                    className="w-full p-4 text-lg rounded bg-gray-700 text-white min-h-[56px]"
-                />
-                <input
-                    type="text"
-                    placeholder="Input 3"
-                    className="w-full p-4 text-lg rounded bg-gray-700 text-white min-h-[56px]"
-                />
-                <input
-                    type="text"
-                    placeholder="Input 4"
-                    className="w-full p-4 text-lg rounded bg-gray-700 text-white min-h-[56px]"
-                />
-                <input
-                    type="text"
-                    placeholder="Input 5"
-                    className="w-full p-4 text-lg rounded bg-gray-700 text-white min-h-[56px]"
-                />
-                
-                {/* Additional five inputs */}
-                <input
-                    type="text"
-                    placeholder="Additional Input 1"
-                    className="w-full p-4 text-lg rounded bg-gray-700 text-white min-h-[56px]"
-                />
-                <input
-                    type="text"
-                    placeholder="Additional Input 2"
-                    className="w-full p-4 text-lg rounded bg-gray-700 text-white min-h-[56px]"
-                />
-                <input
-                    type="text"
-                    placeholder="Additional Input 3"
-                    className="w-full p-4 text-lg rounded bg-gray-700 text-white min-h-[56px]"
-                />
-                <input
-                    type="text"
-                    placeholder="Additional Input 4"
-                    className="w-full p-4 text-lg rounded bg-gray-700 text-white min-h-[56px]"
-                />
-                <input
-                    type="text"
-                    placeholder="Additional Input 5"
-                    className="w-full p-4 text-lg rounded bg-gray-700 text-white min-h-[56px]"
-                />
+            <div className="flex space-x-8 w-full max-w-5xl">
+                <div className="flex flex-col space-y-4 w-full">
+                    <input
+                        type="text"
+                        placeholder="websiteName"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                    <input
+                        type="text"
+                        placeholder="description2"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                    <input
+                        type="text"
+                        placeholder="title"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                    <input
+                        type="text"
+                        placeholder="description"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                    <input
+                        type="text"
+                        placeholder="description"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                    <input
+                        type="text"
+                        placeholder="description"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                    <input
+                        type="text"
+                        placeholder="description"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                </div>
+                <div className="flex flex-col space-y-4 w-full">
+                    <input
+                        type="text"
+                        placeholder="description1"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                    <input
+                        type="text"
+                        placeholder="description3"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                    <input
+                        type="text"
+                        placeholder="title"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                    <input
+                        type="text"
+                        placeholder="title"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                    <input
+                        type="text"
+                        placeholder="title"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                    <input
+                        type="text"
+                        placeholder="title"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                    <input
+                        type="text"
+                        placeholder="title"
+                        className="w-full p-6 text-xl rounded bg-gray-700 text-white min-h-[64px]"
+                    />
+                </div>
             </div>
         </div>
     );
